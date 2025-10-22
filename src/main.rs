476a2d8eaab41372ee272431eaa27719b18955fe
tskoyo <jovansr@protonmail.com>
2025-10-22 19:15:19 +0200

    Add all the commands

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Ok;
 use std::collections::HashMap;
+use std::fmt::format;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::{TcpListener, TcpStream};
 
@@ -26,7 +27,7 @@ async fn main() -> anyhow::Result<()> {
     }
 }
 
-async fn handle_connection(mut socket: TcpStream, db: Db) -> anyhow::Result<()> {
+async fn handle_connection(mut socket: TcpStream, mut db: Db) -> anyhow::Result<()> {
     let (reader, mut writer) = socket.split();
 
     let mut buf_reader = BufReader::new(reader);
@@ -50,28 +51,39 @@ async fn handle_connection(mut socket: TcpStream, db: Db) -> anyhow::Result<()> 
                 writer.write_all(b"+PONG\r\n").await?;
             }
             "GET" => {
-                println!("Command is: GET");
+                let key = parts.get(1).unwrap_or(&"");
+                if let Some(value) = db.get(*key) {
+                    writer
+                        .write_all(format!("{} {}\r\n", value.len(), value).as_bytes())
+                        .await?;
+                } else {
+                    writer.write_all(b"$-1\r\n").await?;
+                }
             }
             "SET" => {
-                println!("Command is: SET");
+                let key = parts[1].to_owned();
+                let value = parts[2].to_owned();
+                db.insert(key, value);
             }
             "DEL" => {
-                println!("Command is: DEL");
+                let key = parts[1];
+                db.remove(key);
             }
             "EXISTS" => {
-                println!("Command is: EXISTS");
+                let key = parts[1];
+                let mut value = 0;
+                if db.contains_key(key) {
+                    value = 1;
+                }
+
+                writer
+                    .write_all(format!("{}\r\n", value).as_bytes())
+                    .await?;
             }
             _ => {
                 writer.write_all(b"-ERR unknown command\r\n").await?;
             }
         }
-
-        // let trimmed = line.trim().to_uppercase();
-        // if trimmed == "PING" {
-        //     writer.write_all(b"+PONG\r\n").await?;
-        // } else {
-        //     writer.write_all(b"-ERR unknown command\r\n").await?;
-        // }
     }
 
     Ok(())
